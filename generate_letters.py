cc8ff2b4949be375b7cf23319987c4a045c99df2
PirateKaiju <santosgonsalvesjr@gmail.com>
2020-09-04 11:17:04 -0300

    Enhanced sound generation with new source files and enabled speeded sound

--- generate_letters.py
+++ generate_letters.py
@@ -11,3 +11,5 @@ lang = 'en'
 for letter in alphabet:
     gtts_obj = gTTS(text=letter, lang=lang, slow=False)
     gtts_obj.save('./res/'+letter+'.mp3')
+
+    os.popen("sox " + './res/' + letter + ".mp3 " + './res/' + letter + ".mp3 silence 1 0.1 0.1%"+" reverse silence 1 0.1 0.1%"+" reverse")
--- ttanimalese.py
+++ ttanimalese.py
@@ -4,15 +4,19 @@ import functools
 
 from pydub import AudioSegment
 
-print(os.getcwd())
+#print(os.getcwd())
 
-AudioSegment.converter = r"C:\\Users\\Luiz\\General Workspaces\\Python\\text_to_animalese\\ffmpeg"
+#AudioSegment.converter = r"C:\\Users\\Luiz\\General Workspaces\\Python\\text_to_animalese\\ffmpeg"
 #AudioSegment.ffmpeg = os.getcwd() + "\\ffmpeg\\ffmpeg.exe"
-AudioSegment.ffprobe = "C:\\Users\\Luiz\\General Workspaces\\Python\\text_to_animalese\\ffmpeg\\ffprobe.exe"
+#AudioSegment.ffprobe = "C:\\Users\\Luiz\\General Workspaces\\Python\\text_to_animalese\\ffmpeg\\ffprobe.exe"
 
-text = "this is a test text"
+#text = "this is a test text"
 
-silent = AudioSegment.silent(duration=300)
+text = sys.argv[1]
+
+silent = AudioSegment.silent(duration=400)
+
+#TODO: CHANGE FROM RAW LETTERS TO PHONEMES
 
 arr_sound = []
 
@@ -23,4 +27,8 @@ for letter in text:
         arr_sound.append(AudioSegment.from_mp3('./res/'+letter+'.mp3'))
 
 result_animalese = functools.reduce(lambda a,b: a+b, arr_sound)
+
+result_animalese = result_animalese._spawn(result_animalese.raw_data, overrides={"frame_rate": int(result_animalese.frame_rate * 1.8)})
+result_animalese.set_frame_rate(result_animalese.frame_rate)
+
 result_animalese.export('./test.mp3', format='mp3')
